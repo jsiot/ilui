a158d91d6d04eaec139ee3848630ce4e162fa277
Jayot <jsdaniot@gmail.com>
2015-10-17 09:53:38 +0700

    Attach button listener to convert image to hex

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 	'use strict';
 
   	var content = document.getElementById('main');
+  	var img = document.getElementById('imageToConvert');
+
 	var remote = require('remote').require('./lib/convert.js');
     
 	document.addEventListener('dragover', function (e) {
@@ -17,10 +19,20 @@
         return false;
     }, false)	
 
+    var imgFile = null;
+
 	document.addEventListener('drop' , function (e) {
 	    e.preventDefault();
 	    var file = e.dataTransfer.files[0];
-	    remote.convert(file.path);
+	    img.src = file.path;
+	    imgFile = file.path;
 	    return false;
 	}, false);
+
+	var convertBtn = document.getElementById('convertBtn');
+	convertBtn.addEventListener('click', function(){
+		if(imgFile != null) {
+			remote.convert(imgFile);	   
+		}
+	})
 })()
