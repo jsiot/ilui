da41d00c4f9e2f47c7f88cd3b42381870b245bd0
Jayot <jsdaniot@gmail.com>
2015-10-18 09:05:05 +0700

    Get file properties

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 (function(){
 	'use strict';
 
+    var path = require('path');
+    var fs = require('fs');
+    var sizeOf = require('image-size');
+    var filesize = require('filesize');
+
     var core = require('./lib/convert.js');
   	var dialog = require('remote').require('./lib/util.js');
-
-  	var content = document.getElementById('main');
-  	var img = document.getElementById('imageToConvert');
-  	
-    var imgFile = null;
     
+  	var content = document.getElementById('main');
+  	var img = document.getElementById('imagetoconvert');
+    var imgFile = null;
+
+  	img.src = 'no.image.png';
+  	    
 	document.addEventListener('dragover', function (e) {
 		e.preventDefault();
 	    return false;
@@ -43,6 +49,7 @@
 				  $scope.tmpFile = data.path;
 				  console.log(data.path);  
 				  document.getElementById('hex').value = data.content;
+				  document.getElementById('status').innerHTML = data.path;
 				}
 			  });	   
 		    }
@@ -60,8 +67,14 @@
     		dialog.openFile(function(s){
               img.src = s[0];
               imgFile = s[0];
+              var d = sizeOf(imgFile);
+              var stats = fs.statSync(imgFile);
+              document.getElementById('hex').value = '';
+			  document.getElementById('status').innerHTML = '';
+              document.getElementById('imgname').innerHTML = path.basename(imgFile);
+              document.getElementById('imgdim').innerHTML = d.width+'x'+d.height; 
+              document.getElementById('imgsize').innerHTML = filesize(stats['size'], {round: 0});
     		})
     	}
     }]);
-
 })()
