913662af6fb039a52980ee9da49dab9e752e869f
Jayot <jsdaniot@gmail.com>
2015-10-18 09:03:05 +0700

    Change window title according to package.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var app = require('app'); 
 var BrowserWindow = require('browser-window');
 var mainWindow = null;
+var info = require('./package.json');
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -17,7 +18,8 @@ app.on('ready', function() {
     'min-width': 500,
     'min-height': 200,
     'accept-first-mouse': true,
-    'title-bar-style': 'hidden'
+    'title-bar-style': 'hidden',
+    title: info.name +' v'+info.version
   });
 
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
