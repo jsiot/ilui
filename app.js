5c788b3de1e1f15662b48440de0d7f6caf94a190
Jayot <jsdaniot@gmail.com>
2015-10-19 08:32:28 +0700

    save and restoring window state

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const app = require('app'); 
 const BrowserWindow = require('browser-window');
 const info = require('./package.json');
+const prefs = require('./lib/prefs.js');
 
 let mainWindow = null;
 
@@ -12,17 +13,44 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('ready', () => {
+app.on('ready', () => { 
+  
+  var wstate = prefs.get('windowState');
+
+  if(wstate === null){
+    wstate = {
+      width: 640,
+      height: 510,
+      maximized: false
+    }
+  }
+
   mainWindow = new BrowserWindow({
-    width: 640,
-    height: 510,
+    width: wstate.width,
+    height: wstate.height,
     'min-width': 500,
     'min-height': 200,
     'accept-first-mouse': true,
     'title-bar-style': 'hidden',
-    title: info.name +' v'+info.version
+    title: info.name +' - '+info.description
   });
 
+  if (wstate.maximized) {
+    mainWindow.maximize();
+  }
+
   mainWindow.loadUrl(`file://${__dirname}/index.html`);
+  
+  mainWindow.on('close', () => {
+    let bounds = mainWindow.getBounds();
+    prefs.set('windowState', {
+      x: bounds.x,
+      y: bounds.y,
+      width: bounds.width,
+      height: bounds.height,
+      maximized: mainWindow.isMaximized()
+    })
+  });
+
   mainWindow.on('closed', () => mainWindow = null);
 });
