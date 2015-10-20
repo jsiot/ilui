5db8dc7c5b4a4f21113581c76faeeb313a1b1f97
Jayot <jsdaniot@gmail.com>
2015-10-20 07:53:14 +0700

    Set autoconvert  flag to true if checkbox is checked

--- js/main.js
+++ js/main.js
@@ -11,54 +11,57 @@
   	const dialog = require('remote').require('./lib/util.js');
     
   	const content = document.getElementById('main');
-  	const img = document.getElementById('imagetoconvert');
-    
-    let imgFile = null;
-
-  	img.src = 'no.image.png';
-  	    
-	document.addEventListener('dragover', (e) => {
-		e.preventDefault();
-	    return false;
-	}, false);
-	
-	document.addEventListener('dragleave', () => false, false);
-    document.addEventListener('dragend', () => false, false)	
-	document.addEventListener('drop' , (e) => {
-	    e.preventDefault();
-	    let file = e.dataTransfer.files[0];
-	    img.src = file.path;
-	    imgFile = file.path;
-	    setProperties(imgFile);
-	    return false;
-	}, false);
-
-    document.getElementById('hex').addEventListener('click', function(){
-        this.select();
-    })
-
-	function setProperties(imageFile) {
-        let d = sizeOf(imageFile);
-        let stats = fs.statSync(imageFile);
-        document.getElementById('hex').value = '';
-	    document.getElementById('status').innerHTML = '';
-        document.getElementById('imgname').innerHTML = path.basename(imgFile);
-        document.getElementById('imgdim').innerHTML = d.width+'x'+d.height; 
-        document.getElementById('imgsize').innerHTML = filesize(stats['size'], {round: 0});
-    }
+  	const img = document.getElementById('imagetoconvert');    
 
 	angular.module('img2lcd', []).controller('AppController', ['$scope', ($scope) => {
-    	$scope.tmpFile;
+    	$scope.autoconvert = false;
+        $scope.tmpFile = null;
+
+        img.src = 'no.image.png';
+        
+        document.addEventListener('dragover', (e) => {
+          e.preventDefault();
+          return false;
+        }, false);
+    
+        document.addEventListener('dragleave', () => false, false);
+        document.addEventListener('dragend', () => false, false)    
+        document.addEventListener('drop' , (e) => {
+          e.preventDefault();
+          let file = e.dataTransfer.files[0];
+          img.src = file.path;
+          $scope.imgFile = file.path;
+          setProperties($scope.imgFile);
+          if($scope.autoconvert){
+            $scope.convert();
+          }
+          return false;
+
+        }, false);
+
+        document.getElementById('hex').addEventListener('click', function(){
+          this.select();
+        })
+
+        function setProperties(imageFile) {
+          let d = sizeOf(imageFile);
+          let stats = fs.statSync(imageFile);
+          document.getElementById('hex').value = '';
+          document.getElementById('status').innerHTML = '';
+          document.getElementById('imgname').innerHTML = path.basename($scope.imgFile);
+          document.getElementById('imgdim').innerHTML = d.width+'x'+d.height; 
+          document.getElementById('imgsize').innerHTML = filesize(stats['size'], {round: 0});
+        }
 
     	$scope.convert = () => {
-    		if(imgFile != null) {
-			  core.convert(imgFile).then( (data) => {
-			  	$scope.tmpFile = data.path;
-				document.getElementById('hex').value = data.content;
-				document.getElementById('status').innerHTML = data.path;
-			  }, (err) => console.log(err))   
-		    }
-    	}
+            if($scope.imgFile != null) {
+              core.convert($scope.imgFile).then( (data) => {
+                $scope.tmpFile = data.path;
+                document.getElementById('hex').value = data.content;
+                document.getElementById('status').innerHTML = data.path;
+              }, (err) => console.log(err))   
+            }
+        }
 
     	$scope.save = () => {
     		if($scope.tmpFile){
@@ -77,6 +80,14 @@
     	$scope.copyToClipboard = () => {
             clipboard.writeText(document.getElementById('hex').value);
             document.getElementById('status').innerHTML = 'Hex copied to clipboard!';
+        }
+
+        $scope.checked = function() {
+            if(document.getElementById('autocb').checked) {
+              $scope.autoconvert = true;
+            } else {
+              $scope.autoconvert = false;
+            }
         }
     }]);
 })()
