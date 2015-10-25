f856e4fd9c1bf267c807645c0483ff4d95459be3
Jayot <jsdaniot@gmail.com>
2015-10-25 09:00:51 +0700

    Add build file

--- build.js
+++ build.js
@@ -1,18 +1,46 @@
+'use strict';
+
 const packager = require('electron-packager');
 const appInfo = require('./app/package.json');
+const path = require('path');
+const rmdir = require('rmdir');
+const fs = require('fs');
+const machine = require('./platform.json') 
+const targz = require('tar.gz');
 
 const opts = {
 	dir: './app',
 	name: appInfo.name,
-	platform: 'linux',
-	arch: 'x64',
-	version: '0.33.8'
+	platform: machine.platform,
+	arch: ["x64"],
+	version: '0.33.8',
+	out: './dist'
 }
 
-packager(opts, function done (err, appPath) {
+packager(opts, (err, appPath) => {
     if(err) {
     	console.log(err);
     } else {
-    	console.log(appPath);
+    	  for(let i in opts.platform){
+          const name = `${opts.name}-${opts.platform[i]}-${opts.arch[0]}`;
+          const build_path = path.join(__dirname, opts.out, name);
+          const lpath = path.join(build_path, 'resources'); 
+          const default_app = path.join(lpath, 'default_app'); 
+        
+          fs.readdir(default_app, (err, files) => {
+            if(!err) {
+              rmdir(default_app, (err, dirs, files) => {
+                if(err) {
+                  console.log(err);
+                } else {
+                  console.log('Cleanup default_app done');
+                }
+              });
+            }
+          })
+          
+          let read = targz().createReadStream(build_path)
+          read.pipe(fs.createWriteStream(path.join(__dirname,'release', `${name}.tar.gz`)));
+        }
     }
 })
