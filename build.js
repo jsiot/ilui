33ab09b34a8c2a5203ada4c1dc60183407e89a63
Jayot <jsdaniot@gmail.com>
2015-10-26 20:12:03 +0700

    Build based on setting on platform.json

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ const opts = {
   dir: './app',
   name: appInfo.name,
   platform: machine.platform,
-  arch: ['x64'],
+  arch: machine.arch,
   version: '0.33.8',
   out: './dist'
 }
@@ -21,26 +21,28 @@ packager(opts, (err, appPath) => {
   if (err) {
     console.log(err)
   } else {
-    for (let i in opts.platform) {
-      const name = `${opts.name}-${opts.platform[i]}-${opts.arch[0]}`
-      const build_path = path.join(__dirname, opts.out, name)
-      const lpath = path.join(build_path, 'resources')
-      const default_app = path.join(lpath, 'default_app')
+    for (let a in opts.arch) {
+      for (let i in opts.platform) {
+        const name = `${opts.name}-${opts.platform[i]}-${opts.arch[a]}`
+        const build_path = path.join(__dirname, opts.out, name)
+        const lpath = path.join(build_path, 'resources')
+        const default_app = path.join(lpath, 'default_app')
 
-      fs.readdir(default_app, (err, files) => {
-        if (!err) {
-          rmdir(default_app, (err, dirs, files) => {
-            if (err) {
-              console.log(err)
-            } else {
-              console.log('Cleanup default_app done')
-            }
-          })
-        }
-      })
+        fs.readdir(default_app, (err, files) => {
+          if (!err) {
+            rmdir(default_app, (err, dirs, files) => {
+              if (err) {
+                console.log(err)
+              } else {
+                console.log('Cleanup default_app done')
+              }
+            })
+          }
+        })
 
-      let read = targz().createReadStream(build_path)
-      read.pipe(fs.createWriteStream(path.join(__dirname, 'release', `${name}.tar.gz`)))
+        let read = targz().createReadStream(build_path)
+        read.pipe(fs.createWriteStream(path.join(__dirname, 'release', `${name}.tar.gz`)))
+      }
     }
   }
 })
